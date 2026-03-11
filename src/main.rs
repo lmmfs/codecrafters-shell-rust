a8a309ca4fa39f42c4d6da977c0a7a345e047bfc
Sir Panda <lmmfs.1998@gmail.com>
2026-03-11 20:01:24 +0000

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,13 @@
 #[allow(unused_imports)]
 use std::io::{self, Write};
 
+use regex::Regex;
+
 fn main() {
+
+    let echo_re = Regex::new(r"^(echo)").unwrap();
+
+
     loop {
         print!("$ ");
         io::stdout().flush().unwrap();
@@ -10,9 +16,16 @@ fn main() {
         io::stdin().read_line(&mut command).unwrap();
 
         match command.trim() {
+            s if echo_re.is_match(s) => echo_command(s.trim_start_matches("echo")),
             "exit" => break,
             "help" => println!("Available commands: help, exit"),
             _ => print!("{}: command not found\n", command.trim()),
         }
     }
+}
+
+
+fn echo_command(args: &str) {
+
+    println!("{}", args);
 }
