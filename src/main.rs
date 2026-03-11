e835ced75e9ae6a84f59653ffae2fff6c8f6cc76
Sir Panda <lmmfs.1998@gmail.com>
2026-03-11 20:08:42 +0000

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,7 @@ use regex::Regex;
 fn main() {
 
     let echo_re = Regex::new(r"^(echo)").unwrap();
+    let type_re = Regex::new(r"^(type)").unwrap();
 
 
     loop {
@@ -17,8 +18,9 @@ fn main() {
 
         match command.trim() {
             s if echo_re.is_match(s) => echo_command(s.trim_start_matches("echo")),
+            s if type_re.is_match(s) => type_command(s.trim_start_matches("type")),
             "exit" => break,
-            "help" => println!("Available commands: help, exit"),
+            "help" => println!("Available commands: help, exit, type, echo"),
             _ => print!("{}: command not found\n", command.trim()),
         }
     }
@@ -28,4 +30,12 @@ fn main() {
 fn echo_command(args: &str) {
     let s = args.strip_prefix(' ').unwrap_or(args);
     println!("{}", s);
+}
+
+fn type_command(args: &str) {
+    let s = args.strip_prefix(' ').unwrap_or(args);
+    match s {
+        "exit" | "help" | "echo" | "type" => println!("{} is a built-in command", s),
+        _ => println!("{}: not found", s),
+    }
 }
