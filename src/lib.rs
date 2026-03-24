d7450ba16ef7d30f5e79381bada64899fde65080
SirPanda <lmmfs.1998@gmail.com>
2026-03-24 22:51:37 +0000

    codecrafters submit [skip ci]

--- src/lib.rs
+++ src/lib.rs
@@ -3,18 +3,24 @@ use std::io::{self, Write};
 
 use anyhow::Result;
 
-use crate::{custom_errors::CustomError, utils::{get_user_input, print_error, prompt}};
+use crate::{builtins::Builtin, custom_errors::CustomError, utils::{get_user_input, print_error, prompt}};
 
 mod utils;
 mod custom_errors;
+mod builtins;
 
 pub fn run() -> Result<()>{
     loop {
         prompt();
         let input = get_user_input().unwrap();
-        let error = CustomError::CommandNotFound(input);
-        print_error(error);
+        let command = Builtin::from(input.as_str());
+        match  command{
+            Builtin::Exit => break,
+            Builtin::NotFound(command_str) => {
+                let error = CustomError::CommandNotFound(command_str);
+                print_error(error);
+            }
+        }
     }
-    #[allow(unreachable_code)]
     Ok(())
 }
